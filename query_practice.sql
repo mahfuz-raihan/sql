c4a4323840a7176beaa320bbe531a7a00e3f36f3
mahfuz-raihan <mahfuz_raihan@outlook.com>
2023-05-06 20:43:29 +0600

    order by keyword

--- query_practice.sql
+++ query_practice.sql
@@ -14,4 +14,45 @@ select column1, column2
 from table_name
 where condition;
 
+-- SQL AND, OR and NOT Operators
+-- AND syntax
+select column1, column2,...
+from table_name
+where condition1 AND condition2 AND condition3;
 
+-- example
+select * from Customers
+where Country='Germany' and City='Berlin';
+
+-- OR systax
+select column1, column2,...
+from table_name
+where condition1 OR condition2 OR condition3;
+--example
+SELECT * FROM Customers
+WHERE City='Berlin' OR City='München';
+
+SELECT * FROM Customers
+WHERE Country='Germany' OR Country='Spain';
+
+-- NOT systax
+select column1, column2,...
+from table_name
+where NOT condition1;
+
+-- example
+SELECT * FROM Customers
+WHERE NOT Country='Sweden';
+
+-- Conbining AND, OR and NOT
+SELECT * FROM Customers
+WHERE NOT Country='Germany' AND NOT Country='USA';
+
+-- SQL order by keyword
+select column1, column2,...
+from table_name
+order by column1, column2 ASC|DESC;
+
+-- example
+SELECT * FROM Customers
+ORDER BY Country;
