5b24ad49d18aba9b6acb64b38b0b1a97a678343d
mahfuz-raihan <mahfuz_raihan@outlook.com>
2023-05-06 19:07:03 +0600

    sql query practice

---
+++ query_practice.sql
@@ -0,0 +1,17 @@
+-- sql select statement
+select column1, column2
+from table_name;
+-- example
+select CustomerName, ContactName, Country
+from Customers;
+
+-- sql distict statement
+select distinct column
+from table_name;
+
+-- sql WHERE statement (condition apply)
+select column1, column2
+from table_name
+where condition;
+
+
